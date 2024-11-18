205bfde685b9f61c6899a867dc4f01821818e6da
FerCamarena <fernando.camarena6817@alumnos.udg.mx>
2024-11-17 21:14:33 -0600

    Add: Level_02 class implementation setup
    Added custom method implementations to Level_02 class.
    Removed "raymath" include.
    Fixed Draw method to match new inherited const method.

--- Level_02.h
+++ Level_02.h
@@ -1,20 +1,59 @@
 //Libraries
 #pragma once
-#include "raylib.h"
+
+//User libraries
 #include "Scene.h"
+
+//User libraries
+#include "SceneManager.h"
 
 /*-----------------------------------Class main-----------------------------------------*/
 class Level_02 : public Scene {
+    //Attributes
+    public:
+        //Level variables
+        int ammoLeft = 3;
+
+        //Tilemap variables
+        Texture2D levelTilesheet = LoadTexture("./assets/Tilemaps/spritesheet_tilemap_red.png");
+        Tilemap level = Tilemap(levelTilesheet);
+
+        //Character variables
+        Texture2D charactersTilesheet = LoadTexture("./assets/Entities/spritesheet_characters.png");
+        Character player = Character(charactersTilesheet, {650.0f, 400.0f}, {20.0f, 52.0f}, level);
+
+        //View variables
+        Camera2D mainCamera;
+        View view = View((Vector2){(float)GetScreenWidth(), (float)GetScreenHeight()}, mainCamera, player);
+
+        //Bullets variables
+        std::vector<std::unique_ptr<Bullet>> bulletsList;
+        std::vector<std::unique_ptr<Bullet>> bulletsPool;
+
+        //Enemies variables
+        std::vector<std::unique_ptr<Entity>> enemyList;
+
+        //Cursor variables
+        Texture2D cursorTexture = LoadTexture("./assets/Other/cursor-aim-empty.png");
+        std::vector<Texture2D> cursorTextures = {
+            LoadTexture("./assets/Other/cursor-aim-empty.png"),
+            LoadTexture("./assets/Other/cursor-aim-full.png")
+        };
+        Cursor cursor = Cursor(cursorTexture, {0, 0}, {0, 0}, cursorTextures, mainCamera.zoom, ammoLeft);
+
+    private:
+
     //Methods
     public:
         //Class constructors to create an instance
         Level_02();
         //Class virtual destructor
         virtual ~Level_02() = default;
-        //Method for process all logic
+        //Methods for process all logic
         void Update() override;
+        void InitializeLevel();
         //Method for process all graphics
-        void Draw() override;
+        void Draw() const override;
         
     private:
 
